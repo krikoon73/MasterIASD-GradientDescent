860e263339571087934b8fe0d102af98cdcc7976
Christophe Compain <cc@arista.com>
2020-12-05 12:12:13 +0100

    05/12/2020

--- build.sbt
+++ build.sbt
@@ -3,4 +3,5 @@ version := "1.0"
 scalaVersion := "2.12.12"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.0.1"
 libraryDependencies += "org.apache.logging.log4j" %% "log4j-api-scala" % "11.0"
+libraryDependencies += "com.github.scopt" %% "scopt" % "4.0.0"
 
--- src/main/scala/org/krikoon73/gradient/GradientDescent.scala
+++ src/main/scala/org/krikoon73/gradient/GradientDescent.scala
@@ -1,25 +1,66 @@
+import java.io.File
+
 import org.apache.spark.sql.{SQLContext, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd._
 import org.apache.log4j.{Level, Logger}
+
+import scopt.OptionParser
+
 import org.krikoon73.lib.BaseGradient
 import org.krikoon73.lib.LoggerHelper
 
+case class CommandLineArgs (
+    AppName: String = "", 
+    Master : String = "local[*]",
+    stepsize: Double = 0.01,
+    iterations: Int = 10,
+    sizefeat: Int = 2
+)
+
 object GradientDescent {
     def main(args: Array[String]): Unit = {
-        val conf = new SparkConf().setAppName("TestGradientDescent").setMaster("local[*]")
-        val sc = new SparkContext(conf)
-        sc.setLogLevel("WARN")
-        //val LOG = Logger.getLogger(this.getClass)  
-        
 
-        //LOG.warn("STARTING PROGAM") 
-        LoggerHelper.logWarn("STARTING PROGRAM")
-        val X = (1.0 to 10000).by(2).toArray
-        val dtrain = sc.parallelize(X.map(x=>(5*x+2,Array(x,1.0))))
-        //LOG.warn(s"dtrain : ${dtrain.count()}")
-        LoggerHelper.logWarn(s"dtrain : ${dtrain.count()}")
-        //LOG.warn(s"Result : ${BaseGradient(dtrain,0.00000000001,10000,2).foreach(println)}")
-        LoggerHelper.logWarn(s"Result : ${BaseGradient(dtrain,0.00000000001,10000,2).foreach(println)}")
+        val parser = new OptionParser[CommandLineArgs]("GradientDescent") {
+            head("Gradient Descent example","1.0")
+            opt[String]('a',"appname")
+                .required().valueName("<SPARK Application Name>")
+                .action((x,a) => a.copy(AppName = x))
+                .text("Setting SPARK Application Name is required")
+            opt[String]('m',"master")
+                .required().valueName("<Cluster or Local>")
+                .action((x,m) => m.copy(Master = x))
+                .text("Setting local or cluster execution is required")
+            opt[Double]('s',"stepsize")
+                .required().valueName("<Gradient Descent step>")
+                .action((x,s) => s.copy(stepsize = x))
+                .text("Setting gradient descent step size is required")
+            opt[Int]('i',"iterations")
+                .required().valueName("<Gradient Descent iteration>")
+                .action((x,i) => i.copy(iterations = x))
+                .text("Setting gradient descent iteration is required")
+            opt[Int]('f',"features")
+                .required().valueName("<Gradient Descent features>")
+                .action((x,i) => i.copy(sizefeat = x))
+                .text("Setting gradient descent features is required")
+        }
+
+        parser.parse(args, CommandLineArgs()) match {
+            case Some(config) => {
+                //val conf = new SparkConf().setAppName("TestGradientDescent").setMaster("local[*]")
+                val conf = new SparkConf().setAppName(config.AppName).setMaster(config.Master)
+                val sc = new SparkContext(conf)
+                sc.setLogLevel("WARN")
+
+                LoggerHelper.logWarn("STARTING PROGRAM")
+                val X = (1.0 to 10000).by(2).toArray
+                val dtrain = sc.parallelize(X.map(x=>(5*x+2,Array(x,1.0))))
+        
+                LoggerHelper.logWarn(s"dtrain : ${dtrain.count()}")
+                //LoggerHelper.logWarn(s"Result : ${s.time(BaseGradient(dtrain,0.00000000001,10000,2).foreach(println))}")
+                LoggerHelper.logWarn(s"Result : ${BaseGradient(dtrain,config.stepsize,config.iterations,config.sizefeat).foreach(println)}")
+            } 
+            case None => System.exit(1)
+        }
     }
 }
